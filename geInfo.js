a23ea49bcecfa6d58cfb976c4ff82e1803e3f232
elpatron1133 <elpatron59093@gmail.com>
2022-09-17 13:40:53 -0500

    app terminada con axios

--- geInfo.js
+++ geInfo.js
@@ -1,96 +1,82 @@
 import mostrarInfo from "./mostrarInfo.js";
-
 export function borrado() {
   let content = document.getElementById("content");
   content.innerHTML = "";
 }
-
-const URL = "https://dragon-ball-super-api.herokuapp.com/api/characters";
+const  URL = "https://dragon-ball-super-api.herokuapp.com/api/characters";
 export function obtenerTodoslosPersonajes() {
-  fetch(URL)
-    .then((response) => response.json())
+  axios(URL)
     .then((data) => {
-      borrado();
-      mostrarInfo(data);
-    })
+        borrado();
+        mostrarInfo(data.data)})
     .catch((error) => console.log(error));
 }
-
 export function obtenerAngeles() {
-  fetch(URL)
-    .then((response) => response.json())
-    .then((array) => {
+  axios(URL)
+    .then((data) => {
+      const array = data.data;
       const angeles = array.filter((personaje) => personaje.specie === "Angel");
-      borrado();
+      borrado()
       mostrarInfo(angeles);
     })
     .catch((error) => console.log(error));
 }
-
 export function obtenerSaiyan() {
-  fetch(URL)
-    .then((response) => response.json())
-    .then((array) => {
+  axios(URL)
+  .then(
+    (data) => {
+      const array = data.data;
       const saiyans = array.filter(
         (personaje) => personaje.specie === "Saiyan"
       );
       borrado();
       mostrarInfo(saiyans);
-    });
+    }
+  );
 }
 export function obtenerFusions() {
-  fetch(URL)
-    .then((response) => response.json())
-    .then((array) => {
-      const fusions = array.filter(
-        (personaje) => personaje.specie === "Fusion"
-      );
-      borrado();
-      mostrarInfo(fusions);
-    });
+    axios(URL)
+    .then( (data) => {
+        const array = data.data;
+        const fusions = array.filter((personaje) => personaje.specie === "Fusion");
+        borrado();
+        mostrarInfo(fusions);
+    })
 }
-
 export function obtenerHumans() {
-  fetch(URL)
-    .then((response) => response.json())
-    .then((array) => {
-      const humans = array.filter((personaje) => personaje.specie === "Humano");
-      borrado();
-      mostrarInfo(humans);
-    });
+    axios(URL)
+    .then( (data) => {
+        const array = data.data;
+        const humans = array.filter((personaje) => personaje.specie === "Humano");
+        borrado();
+        mostrarInfo(humans);
+    })
 }
 export function obtenerNamekians() {
-  fetch(URL)
-    .then((response) => response.json())
-    .then((array) => {
-      const humans = array.filter(
-        (personaje) => personaje.specie === "Namekiano"
-      );
-      borrado();
-      mostrarInfo(humans);
-    });
+    axios(URL)
+    .then( (data) => {
+        const array = data.data;
+        const humans = array.filter((personaje) => personaje.specie === "Namekiano");
+        borrado();
+        mostrarInfo(humans);
+    })
 }
 export function obtenerMestizos() {
-  fetch(URL)
-    .then((response) => response.json())
-    .then((array) => {
-      const humans = array.filter(
-        (personaje) => personaje.specie === "Saiyan mestizo"
-      );
-      borrado();
-      mostrarInfo(humans);
-    });
+    axios(URL)
+    .then( (data) => {
+        const array = data.data;
+        const humans = array.filter((personaje) => personaje.specie === "Saiyan mestizo");
+        borrado();
+        mostrarInfo(humans);
+    })
 }
-
 export function obtenerPersonajeAleatorio() {
-  fetch(URL)
-    .then((response) => response.json())
-    .then((array) => {
-      const numeroAleatorio = Math.floor(Math.random() * array.length);
-      const aleatorio = array.filter(
-        (personaje) => personaje.id === numeroAleatorio
-      );
-      borrado();
-      mostrarInfo(aleatorio);
-    });
+    axios(URL)
+    .then( (data) => {
+        const array = data.data;
+        const numeroAleatorio = Math.floor(Math.random() * array.length);
+        const aleatorio = array.filter((personaje) => personaje.id ===   numeroAleatorio);
+        borrado();
+        mostrarInfo(aleatorio);
+    })
 }
