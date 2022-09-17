107f8363cb1c62eb5333d7b7a777f498a65a95ba
elpatron1133 <elpatron59093@gmail.com>
2022-09-17 14:02:20 -0500

    app terminada con axios

--- geInfo.js
+++ geInfo.js
@@ -3,12 +3,13 @@ export function borrado() {
   let content = document.getElementById("content");
   content.innerHTML = "";
 }
-const  URL = "https://dragon-ball-super-api.herokuapp.com/api/characters";
+const URL = "https://dragon-ball-super-api.herokuapp.com/api/characters";
 export function obtenerTodoslosPersonajes() {
   axios(URL)
     .then((data) => {
-        borrado();
-        mostrarInfo(data.data)})
+      borrado();
+      mostrarInfo(data.data);
+    })
     .catch((error) => console.log(error));
 }
 export function obtenerAngeles() {
@@ -16,67 +17,63 @@ export function obtenerAngeles() {
     .then((data) => {
       const array = data.data;
       const angeles = array.filter((personaje) => personaje.specie === "Angel");
-      borrado()
+      borrado();
       mostrarInfo(angeles);
     })
     .catch((error) => console.log(error));
 }
 export function obtenerSaiyan() {
-  axios(URL)
-  .then(
-    (data) => {
-      const array = data.data;
-      const saiyans = array.filter(
-        (personaje) => personaje.specie === "Saiyan"
-      );
-      borrado();
-      mostrarInfo(saiyans);
-    }
-  );
+  axios(URL).then((data) => {
+    const array = data.data;
+    const saiyans = array.filter((personaje) => personaje.specie === "Saiyan");
+    borrado();
+    mostrarInfo(saiyans);
+  });
 }
 export function obtenerFusions() {
-    axios(URL)
-    .then( (data) => {
-        const array = data.data;
-        const fusions = array.filter((personaje) => personaje.specie === "Fusion");
-        borrado();
-        mostrarInfo(fusions);
-    })
+  axios(URL).then((data) => {
+    const array = data.data;
+    const fusions = array.filter((personaje) => personaje.specie === "Fusion");
+    borrado();
+    mostrarInfo(fusions);
+  });
 }
 export function obtenerHumans() {
-    axios(URL)
-    .then( (data) => {
-        const array = data.data;
-        const humans = array.filter((personaje) => personaje.specie === "Humano");
-        borrado();
-        mostrarInfo(humans);
-    })
+  axios(URL).then((data) => {
+    const array = data.data;
+    const humans = array.filter((personaje) => personaje.specie === "Humano");
+    borrado();
+    mostrarInfo(humans);
+  });
 }
 export function obtenerNamekians() {
-    axios(URL)
-    .then( (data) => {
-        const array = data.data;
-        const humans = array.filter((personaje) => personaje.specie === "Namekiano");
-        borrado();
-        mostrarInfo(humans);
-    })
+  axios(URL).then((data) => {
+    const array = data.data;
+    const humans = array.filter(
+      (personaje) => personaje.specie === "Namekiano"
+    );
+    borrado();
+    mostrarInfo(humans);
+  });
 }
 export function obtenerMestizos() {
-    axios(URL)
-    .then( (data) => {
-        const array = data.data;
-        const humans = array.filter((personaje) => personaje.specie === "Saiyan mestizo");
-        borrado();
-        mostrarInfo(humans);
-    })
+  axios(URL).then((data) => {
+    const array = data.data;
+    const humans = array.filter(
+      (personaje) => personaje.specie === "Saiyan mestizo"
+    );
+    borrado();
+    mostrarInfo(humans);
+  });
 }
 export function obtenerPersonajeAleatorio() {
-    axios(URL)
-    .then( (data) => {
-        const array = data.data;
-        const numeroAleatorio = Math.floor(Math.random() * array.length);
-        const aleatorio = array.filter((personaje) => personaje.id ===   numeroAleatorio);
-        borrado();
-        mostrarInfo(aleatorio);
-    })
+  axios(URL).then((data) => {
+    const array = data.data;
+    const numeroAleatorio = Math.floor(Math.random() * array.length);
+    const aleatorio = array.filter(
+      (personaje) => personaje.id === numeroAleatorio
+    );
+    borrado();
+    mostrarInfo(aleatorio);
+  });
 }
